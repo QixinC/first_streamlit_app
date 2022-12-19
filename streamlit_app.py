ac611329022c72537baca81bb9d43bc01c92e28f
QixinC <37123920+QixinC@users.noreply.github.com>
2022-12-18 22:27:27 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,3 @@
-
 import streamlit
 import pandas
 import requests
@@ -24,25 +23,38 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
-streamlit.dataframe(fruits_to_show)
+# streamlit.dataframe(fruits_to_show)
+
+#create the repeatable code block (called a function)
+def get_fruityvice_data(this_fruit_choice):
+   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+   return fruityvice_normalized
+ 
 
 #New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
-    streamlit.error("please select a fruit to get informaation.")
+      streamlit.error("please select a fruit to get information.")
   else:
-      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-#     streamlit.dataframe(fruityvice_normalized)
+      back_from_function = get_fruityvice_data (fruit_choice)
+      streamlit.dataframe(back_from_function)
+ 
+except URLexcept as e:
+    streamlit.err()
+  
+#streamlit.write('The user entered ', fruit_choice)
 
-except URLError as e:
-    streamlit.error()
+# streamlit.text(fruityvice_response.json())
 
+# write your own comment -what does the next line do? 
+# fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do?
+#streamlit.dataframe(fruityvice_normalized)
 
 streamlit.stop()
-
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
